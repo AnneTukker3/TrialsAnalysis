ad583f58dc1b794bc41a05c41acb3124a3db5b1d
AnneTukker3 <amtukker13@gmail.com>
2026-08-26 14:46:57 +0100

    adding replicator function

--- R/Replicator.R
+++ R/Replicator.R
@@ -1,3 +1,8 @@
+table <- data.frame(line = rep(1:30))
+
+table |> dplyr::slice(rep(1:dplyr::n(), each = 3)) |> 
+  dplyr::group_by(line) |> 
+  dplyr::mutate(rep = rep(1:3)) #add column 'rep' to dataframe 'table'
 
 replicator <- function(df, column, replication){
   df |> dplyr::slice(rep(1:dplyr::n(), each = replication)) |> 
