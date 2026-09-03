d9c3abb94611a7b07a56a310e59795dd817d18ed
AnneTukker3 <amtukker13@gmail.com>
2026-09-03 12:25:57 +0100

    adding documentation

--- R/Replicator.R
+++ R/Replicator.R
@@ -1,12 +1,21 @@
-table <- data.frame(line = rep(1:30))
-
-table |> dplyr::slice(rep(1:dplyr::n(), each = 3)) |> 
-  dplyr::group_by(line) |> 
-  dplyr::mutate(rep = rep(1:3)) #add column 'rep' to dataframe 'table'
-
+#' replicating accessions in a list n times
+#' 
+#' @param df A dataframe containing a list of accessions.
+#' @param column the column header for the accession list.
+#' @param replication The number of replications per accession.
+#' 
+#' @return A dataframe containing the original accession list and a replication column.
+#' 
+#' @example
+#' \dontrun{
+#' replicated_dataframe <- replicator(
+#'  df = mydata,
+#'  column = line, # column name, 
+#'  replication = 5 # number of replications
+#'  ) }
+#'  @export
 replicator <- function(df, column, replication){
   df |> dplyr::slice(rep(1:dplyr::n(), each = replication)) |> 
     dplyr::group_by({{column}}) |> #group the df based on "column"
     dplyr::mutate(rep = rep(1:replication)) #adding column 'rep' to df and replicating each "column" "replication" amount of times
 }
-replicator(table, line, 5) #testing if the replicator function works
